34c554a5af1e9d23774326bbda13fa610be4dfd2
Nikita Nikiforov <nikifor38@ya.ru>
2021-12-12 09:56:31 +0700

    Анимация кнопок

--- script/script.js
+++ script/script.js
@@ -1,9 +1,22 @@
-const editButton = document.querySelector('.profile__edit-button');
-const closeButton = document.querySelector('.form__close-icon')
-const form = document.querySelector('.form');
-function toggleForm(){
-    form.classList.toggle('form__opened');
+const editButton = document.querySelector(".profile__edit-button");
+const closeButton = document.querySelector(".form__close-icon");
+const form = document.querySelector(".form");
+function toggleForm() {
+  form.classList.toggle("form__opened");
 }
+editButton.addEventListener("click", toggleForm);
+closeButton.addEventListener("click", toggleForm);
 
-editButton.addEventListener('click', toggleForm);
-closeButton.addEventListener('click', toggleForm);
+let profileName = document.querySelector(".profile__name");
+let profileStatus = document.querySelector(".profile__status");
+const submit = document.querySelector(".form__button");
+let formElement = document.querySelector('.form__container');
+let nameInput = formElement.querySelector('#name');
+let statusInput = formElement.querySelector('#status');
+function formSubmitHandler(event) {
+  event.preventDefault();
+  profileName.textContent = nameInput.value;
+  profileStatus.textContent = statusInput.value;
+}
+formElement.addEventListener('submit', formSubmitHandler);
+
