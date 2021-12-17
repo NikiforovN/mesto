e2a737de47269cc8171866ae126caa416414bda0
Nikita Nikiforov <nikifor38@ya.ru>
2021-12-17 19:04:36 +0700

    BugsFixed

--- script/script.js
+++ script/script.js
@@ -1,28 +1,26 @@
 const editButton = document.querySelector(".profile__edit-button");
-const closeButton = document.querySelector(".form__close-icon");
-const form = document.querySelector(".form");
-function toggleForm() {
-  form.classList.toggle("form__opened");
+const closeButton = document.querySelector(".popup__close-icon");
+const form = document.querySelector(".popup");
+function openForm() {
+  form.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
 }
-editButton.addEventListener("click", toggleForm);
-closeButton.addEventListener("click", toggleForm);
+function closeForm() {
+  form.classList.remove("popup_opened");
+}
+editButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
 
 let profileName = document.querySelector(".profile__name");
 let profileStatus = document.querySelector(".profile__status");
-let formElement = document.querySelector(".form__container");
+let formElement = document.querySelector(".popup__container");
 let nameInput = formElement.querySelector("#name");
 let statusInput = formElement.querySelector("#status");
 function formSubmitHandler(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
   profileStatus.textContent = statusInput.value;
+  closeForm();
 }
 formElement.addEventListener("submit", formSubmitHandler);
-formElement.addEventListener("submit", toggleForm);
-
-function editFormWithoutEdit() {
-  nameInput.value = profileName.textContent;
-  statusInput.value = profileStatus.textContent;
-}
-closeButton.addEventListener("click", editFormWithoutEdit);
-
