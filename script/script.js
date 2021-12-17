56f4a4e1941ce73eb81f665c5ae37c250c8324d6
Nikita Nikiforov <nikifor38@ya.ru>
2021-12-17 19:09:46 +0700

    BugsFixed

--- script/script.js
+++ script/script.js
@@ -1,22 +1,24 @@
 const editButton = document.querySelector(".profile__edit-button");
 const closeButton = document.querySelector(".popup__close-icon");
 const form = document.querySelector(".popup");
-function openForm() {
-  form.classList.add("popup_opened");
-  nameInput.value = profileName.textContent;
-  statusInput.value = profileStatus.textContent;
-}
-function closeForm() {
-  form.classList.remove("popup_opened");
-}
-editButton.addEventListener("click", openForm);
-closeButton.addEventListener("click", closeForm);
-
 let profileName = document.querySelector(".profile__name");
 let profileStatus = document.querySelector(".profile__status");
 let formElement = document.querySelector(".popup__container");
 let nameInput = formElement.querySelector("#name");
 let statusInput = formElement.querySelector("#status");
+function openForm() {
+  form.classList.add("popup_opened");
+  nameInput.value = profileName.textContent;
+  statusInput.value = profileStatus.textContent;
+}
+
+function closeForm() {
+  form.classList.remove("popup_opened");
+}
+
+editButton.addEventListener("click", openForm);
+closeButton.addEventListener("click", closeForm);
+
 function formSubmitHandler(event) {
   event.preventDefault();
   profileName.textContent = nameInput.value;
